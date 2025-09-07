e17b2dfc5eb55f079af145e18822231b1b41cbe8
Brian Koropoff <bkoropoff@gmail.com>
2025-09-07 15:45:05 -0700

    doc: add GitHub link to header

--- doc/conf.py
+++ doc/conf.py
@@ -3,12 +3,21 @@ author = "Brian Koropoff"
 copyright = "2025 Brian Koropoff"
 version = "0.1.0"
 
+templates_path = ['templates']
+
 extensions = [
     "sphinx_lua_ls",
     "myst_parser"
 ]
 
 html_theme = "sphinx_rtd_theme"
+
+html_context = {
+    'display_github': True,
+    'github_user': 'bkoropoff',
+    'github_repo': 'chorus',
+}
+
 lua_ls_project_root = "../lua"
 lua_ls_backend = "emmylua"
 lua_ls_apidoc_format = "md"
