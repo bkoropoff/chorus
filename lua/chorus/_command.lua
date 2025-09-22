f7365069f7a9dfe838610905fa76cb5a9babc108
Brian Koropoff <bkoropoff@gmail.com>
2025-09-22 00:11:28 -0700

    command: use `bufhidden`, not `hidden`

--- lua/chorus/_command.lua
+++ lua/chorus/_command.lua
@@ -6,7 +6,7 @@ function update(fargs)
     'FileType',
     'nvim-pack',
     function(args)
-      vim.opt_local.hidden = false
+      vim.opt_local.bufhidden = 'delete'
       local write_ac = vim.api.nvim_get_autocmds {
         event = 'BufWriteCmd',
         buffer = args.buf
